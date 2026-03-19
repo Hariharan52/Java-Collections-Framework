f84aa45a13e3b5b548015bd59cca60acec95d873
Hariharan52 <m.hariharan522004@gmail.com>
2026-03-19 22:38:41 +0530

    Get method problem

---
+++ ArrayList/GetMethod.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Getmethod {
+    
+    public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    cars.add("Volvo");
+    System.out.print(cars.get(1));
+    }
+}
