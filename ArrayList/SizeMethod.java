a39de8f00a3c141f6eec1db4e9735ae3314c50ce
Hariharan52 <m.hariharan522004@gmail.com>
2026-03-20 11:32:54 +0530

    SizeMethod again added successfully

---
+++ ArrayList/SizeMethod.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class SizeMethod {
+    
+    public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    cars.add("Volvo");
+    System.out.print(cars.size());
+    }
+    
+}
